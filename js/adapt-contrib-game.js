c49cac0349d25e1b3b344b0a3201c3321bf6196a
Ilya703 <72661678+Ilya703@users.noreply.github.com>
2021-02-23 15:27:16 +0300

    8

--- js/adapt-contrib-game.js
+++ js/adapt-contrib-game.js
@@ -4,56 +4,46 @@ define([
   'core/js/models/componentModel'
 ], function(Adapt, ComponentView, ComponentModel) {
 
-  class TextView extends ComponentView {
+  class GraphicView extends ComponentView {
 
     preRender() {
+      this.listenTo(Adapt, 'device:changed', this.resizeImage);
+
       this.checkIfResetOnRevisit();
     }
 
     postRender() {
-      this.setReadyStatus();
-
-      this.setupInview();
-    }
-
-    setupInview() {
-      const selector = this.getInviewElementSelector();
-      if (!selector) {
-        this.setCompletionStatus();
-        return;
-      }
-
-      this.setupInviewCompletion(selector);
-    }
-
-    /**
-     * determines which element should be used for inview logic - body, instruction or title - and returns the selector for that element
-     */
-    getInviewElementSelector() {
-      if (this.model.get('body')) return '.component__body';
-
-      if (this.model.get('instruction')) return '.component__instruction';
-
-      if (this.model.get('displayTitle')) return '.component__title';
-
-      return null;
+      this.resizeImage(Adapt.device.screenSize, true);
     }
 
     checkIfResetOnRevisit() {
       const isResetOnRevisit = this.model.get('_isResetOnRevisit');
 
-      // If reset is enabled set defaults
       if (isResetOnRevisit) {
         this.model.reset(isResetOnRevisit);
       }
     }
+
+    resizeImage(width, setupInView) {
+      const imageWidth = width === 'medium' ? 'small' : width;
+      const imageSrc = (this.model.get('_game')) ? this.model.get('_game')[imageWidth] : '';
+      this.$('.js-game-set-image-src').attr('src', imageSrc);
+
+      this.$('.game__widget').imageready(() => {
+        this.setReadyStatus();
+
+        if (setupInView) {
+          this.setupInviewCompletion('.game__widget');
+        }
+      });
+    }
   }
 
-  TextView.template = 'text';
+  GraphicView.template = 'game';
 
   return Adapt.register('game', {
     model: ComponentModel.extend({}),// create a new class in the inheritance chain so it can be extended per component type if necessary later
-    view: TextView
+    view: GraphicView
   });
-});
 
+});
