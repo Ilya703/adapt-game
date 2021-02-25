94959d42e72a79c526cb6e63947e3725045d27a9
Ilya703 <72661678+Ilya703@users.noreply.github.com>
2021-02-25 21:28:44 +0300

    Update adapt-contrib-game.js

--- js/adapt-contrib-game.js
+++ js/adapt-contrib-game.js
@@ -45,6 +45,4 @@ define([
     model: ComponentModel.extend({}),// create a new class in the inheritance chain so it can be extended per component type if necessary later
     view: GraphicView
   });
-  let h = 3;
-  document.getElementById('text').innerHTML = h;
 });
