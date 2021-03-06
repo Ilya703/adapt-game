7a257fa2684e8027063b1d2e43722a8b97f89a43
Ilya703 <72661678+Ilya703@users.noreply.github.com>
2021-03-06 11:45:08 +0300

    Update adapt-contrib-game.js

--- js/adapt-contrib-game.js
+++ js/adapt-contrib-game.js
@@ -19,12 +19,28 @@ define([
       var button1 = document.querySelector(".button_1");
       var button2 = document.querySelector(".button_2");
       var count = document.querySelector(".count");
-      function game() {
-        count.innerHTML = "9";
+      var text = document.querySelector(".text_1");
+      let i = 0;
+      let set = ['Домашний театр','Гардероб официантов','Шубохранилище','Аквадискотека','Компьютерный клуб'];
+      function game1() {
+        i += 1;
+        text.innerHTML = set[i];
+        if (i == 0 or i == 1 or i == 3){
+          count += 1;
+        }
+        count.innerHTML = count;
+      };
+      function game2() {
+        i += 1;
+        text.innerHTML = set[i];
+        if (i == 2 or i == 4){
+          count += 1;
+        }
+        count.innerHTML = count;
       };
 
-      button1.addEventListener("click", game);
-      button2.addEventListener("click", game);
+      button1.addEventListener("click", game1);
+      button2.addEventListener("click", game2);
     }
 
     checkIfResetOnRevisit() {
@@ -47,32 +63,6 @@ define([
           this.setupInviewCompletion('.game__widget');
         }
       });
-    }
-    game() {
-      const game = this.model.get('_game');
-      let counter = 0;
-      let i = 1;
-      let set = [a1,a2,a3,a4,a5];
-      let a1 = {
-        title: 'Домашний театр',
-        isTrue: true,
-      }
-      let a2 = {
-        title: 'Гардероб официантов',
-        isTrue: true,
-      }
-      let a3 = {
-        title: 'Шубохранилище',
-        isTrue: false,
-      }
-      let a4 = {
-        title: 'Аквадискотека',
-        isTrue: true,
-      }
-      let a5 = {
-        title: 'Компьютерный клуб',
-        isTrue: false,
-      }
     }
   }
 
