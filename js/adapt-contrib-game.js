4c65d7da31f288e946b50718cc41c4d6db01bb8a
Ilya703 <72661678+Ilya703@users.noreply.github.com>
2021-03-12 20:26:15 +0300

    Update adapt-contrib-game.js

--- js/adapt-contrib-game.js
+++ js/adapt-contrib-game.js
@@ -37,8 +37,16 @@ define([
         i += 1;
         el = document.querySelectorAll(".text_")[i];
         el.classList.remove("is_hide");
-        if (i == 5){
+        if (i == 4){
         	inner.classList.add("show");
+          	el = document.querySelectorAll(".text_")[i];
+          	el.classList.add("is_hide");
+          	if (counter < 3){
+            	phrase.innerHTML = 'Мне удалось заблудиться во «дворце Путина». Ищите меня на складе грязи!';
+          	} else {
+            	phrase.innerHTML = 'Я знаю, как устроен «дворец Путина». Ищите меня в spa-капсуле';
+          	};
+          	num.innerHTML = `${counter} из 5`;
         };
       };
       function game2() {
@@ -51,8 +59,16 @@ define([
         i += 1;
         el = document.querySelectorAll(".text_")[i];
         el.classList.remove("is_hide");
-        if (i == 5){
+        if (i == 4){
         	inner.classList.add("show");
+          	el = document.querySelectorAll(".text_")[i];
+          	el.classList.add("is_hide");
+          	if (counter < 3){
+            	phrase.innerHTML = 'Мне удалось заблудиться во «дворце Путина». Ищите меня на складе грязи!';
+          	} else {
+            	phrase.innerHTML = 'Я знаю, как устроен «дворец Путина». Ищите меня в spa-капсуле';
+          	};
+          	num.innerHTML = `${counter} из 5`;
         };
       };
       function more(){
