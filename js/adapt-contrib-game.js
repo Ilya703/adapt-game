f9d2a0ae4d11df24ee67c0fa5a7708aec02e2c88
Ilya703 <72661678+Ilya703@users.noreply.github.com>
2021-03-26 20:43:33 +0300

    Update adapt-contrib-game.js

--- js/adapt-contrib-game.js
+++ js/adapt-contrib-game.js
@@ -21,6 +21,12 @@ define([
     postRender() {
       this.resizeImage(Adapt.device.screenSize, true);
 
+      this.setReadyStatus();
+
+      if (this.model.get('_setCompletionOn') === 'inview') {
+        this.setupInviewCompletion();
+      }
+
       var button1 = document.querySelector(".button_1");
       var button2 = document.querySelector(".button_2");
       var count = document.querySelector(".count");
