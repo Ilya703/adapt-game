6ec42ac6aaa1b919ab5f244f14e423f4667d194e
Ilya703 <72661678+Ilya703@users.noreply.github.com>
2021-04-07 13:25:36 +0300

    Update adapt-contrib-game.js

--- js/adapt-contrib-game.js
+++ js/adapt-contrib-game.js
@@ -67,7 +67,6 @@ define([
         	inner1.classList.add("show");
           progress.classList.add("w1");
           m += 1;
-          console.log(m);
           	if (counter < 3){
             	phrase.innerHTML = 'Мне удалось заблудиться во «дворце Путина». Ищите меня на складе грязи!';
           	} else {
@@ -91,7 +90,7 @@ define([
         count.innerHTML = counter;
       };
       function progress1(){
-        if (document.querySelector('[aria-label=" Incomplete. Game"] .pagelevelprogress__indicator .pagelevelprogress__indicator-inner .pagelevelprogress__indicator-bar').classList.add('w') == null){
+        if (document.querySelector('[aria-label=" Incomplete. Game"] .pagelevelprogress__indicator .pagelevelprogress__indicator-inner .pagelevelprogress__indicator-bar') == null){
           console.log(document.querySelector('[aria-label=" Completed. Game"] .pagelevelprogress__indicator .pagelevelprogress__indicator-inner .pagelevelprogress__indicator-bar').classList.add('w'));
         } else {
           console.log(document.querySelector('[aria-label=" Incomplete. Game"] .pagelevelprogress__indicator .pagelevelprogress__indicator-inner .pagelevelprogress__indicator-bar').classList.add('w'));
